a87d045893917a3a002c4afcb605c279a0618696
Khoi Tran <emails.khoi@gmail.com>
2014-08-18 19:11:37 -0700

    Importing front end and create food model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Restaurant::Application.config.secret_key_base = '968c86f75c07d6f1983dfcbf01e2b103eb78675f9fb908b0e4185b9597bc9ed8a94fdb85dad86d342d3b0a113ff64201187aa9f5d9b79316df894900079576bd'
---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Food.create(name: 'Waffle Station', price: 5.50, category: 'Breakfast')
+Food.create(name: 'Scrambled Eggs', price: 10.50, category: 'Breakfast')
+Food.create(name: 'Buttermilk Pancakes', price: 10.50, category: 'Breakfast')
+Food.create(name: 'Crispy Bacon', price: 10.50, category: 'Breakfast')
+Food.create(name: 'Basil Infused Cous Cous ', price: 12.50, category: 'Lunch')
+Food.create(name: 'Steamed Brown Rice ', price: 12.50, category: 'Lunch')
